34918ca0e75bdae6ed615d07a3ff4ac1b1401de6
Felix Satyaputra <felix.satyaputra@typesafe.com>
2017-11-30 14:55:31 +1100

    conductr-cli 1.2.25

--- Formula/conductr-cli.rb
+++ Formula/conductr-cli.rb
@@ -1,9 +1,9 @@
 class ConductrCli < Formula
   desc "The Lightbend ConductR CLI"
   homepage "https://conductr.lightbend.com"
-  url "https://bintray.com/lightbend/generic/download_file?file_path=conductr-cli-1.2.23-Mac_OS_X-x86_64.zip"
-  version "1.2.23"
-  sha256 "7dc9581419321794409febcf4677612f8601985b4be461d836fb1cb964e2b9e6"
+  url "https://bintray.com/lightbend/generic/download_file?file_path=conductr-cli-1.2.25-Mac_OS_X-x86_64.zip"
+  version "1.2.25"
+  sha256 "e1a7eb9b0a21a191f871257f92799d372fa16145d39b271a1ef45b8b662e9f3f"
 
   def install
     bin.install Dir["*"]
